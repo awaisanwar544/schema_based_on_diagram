943ed66b78915d6d6cb37a18978edf310a27a173
Kenny <kennysalazar3197@gmail.com>
2022-05-03 17:31:16 -0500

    Create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,12 @@ CREATE TABLE patients (
 	name varchar(250),
 	date_of_birth date
 );
+
+/* Create medical_histories */
+
+CREATE TABLE medical_histories (
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	admitted_at timestamp,
+	patient_id int REFERENCES patients(id),
+  status varchar(250)
+);
